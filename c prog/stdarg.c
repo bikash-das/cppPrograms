9b070d8124c486dfeb26a2e8ece3dd8f738a438b
Bikash Das <dasbikash989@gmail.com>
2019-06-29 03:42:10 +0530

    added comments

--- c prog/stdarg.c
+++ c prog/stdarg.c
@@ -3,19 +3,17 @@
 
 int fun(int n, ...){
   int i,j = 1, val = 0;
-  va_list p; //pointer to an argument list
-  va_start(p,n); //p points at the first var-argument
+  va_list p; // sort of declaring a variable of pointer type to point to the arbitrary list
+  va_start(p,n); // now pointer points to the first element
   for(; j<n; ++j){  //runs for j values 1,2,3, exits when j = 4
-    i = va_arg(p,int);  //we call it with a va-list i.e 'p' and a type, (int)
-     // will return (int) *p => 1 then 2 then 3, since we pass 1,2,3
-     // p += sizeof(int)    and increment the pointer to the next number
-    val += i; //we add the values, i has value pointed by *p which is 1, for 1st iteration, ...
+    i = va_arg(p,int);  //returns the first value and points to next element, *p
+    val += i; //we add the values
   }
   va_end(p); //release the resource
   return val;
 }
 int main(){
   printf("%d\\n",fun(4,1,2,3));
-  printf("\n"); //added 
+  printf("\n"); //added
   return 0;
 }
