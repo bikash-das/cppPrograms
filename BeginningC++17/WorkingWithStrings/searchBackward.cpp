f6c10d31563b169d64d065ca09a36e26b6e6cbcf
Bikash Das <dasbikash989@gmail.com>
2019-07-06 08:29:07 +0530

    Searching string backward using rfind()

---
+++ BeginningC++17/WorkingWithStrings/searchBackward.cpp
@@ -0,0 +1,13 @@
+// using rfind() to search backward
+#include <iostream>
+#include <string>
+
+int main(){
+  std::string sentence {"Manners maketh man"};
+  std::string word {"an"};
+  std::cout << sentence.rfind(word) << std::endl;
+  if(sentence.rfind(std::string {"bik"}) == std::string::npos){
+    std::cout << "Non_existent" << std::endl;
+  }
+  std::cout << sentence.substr(sentence.rfind(word), sentence.length()-sentence.rfind(word)) << std::endl;
+}
