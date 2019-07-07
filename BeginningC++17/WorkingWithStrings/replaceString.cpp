88dcf577d6330e0e0b70cde249c8341202a13180
Bikash Das <dasbikash989@gmail.com>
2019-07-07 14:51:41 +0530

    Made some changes

--- BeginningC++17/WorkingWithStrings/replaceString.cpp
+++ BeginningC++17/WorkingWithStrings/replaceString.cpp
@@ -5,6 +5,8 @@ int main(){
   std::string text {"hello it's me bikash"};
   std::string name {"Mahesh"};
   int find_bikash = text.find(std::string{"bikash"});
-  text.replace(14,std::string{"bikash"}.length(), name);
+  std::cout << "find_bikash: " << find_bikash << std::endl;
+
+  text.replace(find_bikash,std::string{"bikash"}.length(), name);
   std::cout << text << std::endl;
 }
