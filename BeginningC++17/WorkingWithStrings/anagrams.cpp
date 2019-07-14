35b5b2a7ca8c854f82f9685911c1bc828a005a4c
Bikash Das <dasbikash989@gmail.com>
2019-07-14 16:04:43 +0530

    Given two words, check if they are anagrams

---
+++ BeginningC++17/WorkingWithStrings/anagrams.cpp
@@ -0,0 +1,37 @@
+/*
+  Exercise 7-4. Write a program that prompts for the input of two words and determines
+  whether one is an anagram of the other. An anagram of a word is formed by
+  rearranging its letters, using each of the original letters precisely once. For instance,
+  listen and silent are anagrams of one another, but listens and silent are not.
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+
+int main(){
+  std::string word_1 {};
+  std::cout << "Enter first word: ";
+  std::cin >> word_1;
+
+  std::string word_2 {};
+  std::cout << "Enter second word: ";
+  std::cin >> word_2;
+
+  if(word_1.length() == word_2.length()){
+    std::map<char, int> word_1_map{};
+    std::map<char, int> word_2_map{};
+    for(auto& c: word_1)
+      word_1_map[std::tolower(c)]++;
+    for(auto& c: word_2)
+      word_2_map[std::tolower(c)]++;
+    if(word_1_map == word_2_map){
+      std::cout << "Anagrams" << std::endl;
+    }
+    else{
+      std::cout << "Not Anagrams" << std::endl;
+    }
+  }else{
+    std::cout << "Length Mismatch" << std::endl;
+  }
+}
