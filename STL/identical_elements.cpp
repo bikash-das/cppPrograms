fe07ddb23253cc6d021490a1912590ac735d1bee
Bikash Das <dasbikash989@gmail.com>
2019-08-15 12:41:49 +0530

    taking input using istream_iterator and using erase() instead of resize

--- STL/identical_elements.cpp
+++ STL/identical_elements.cpp
@@ -4,44 +4,26 @@
 */
 #include <iostream>
 #include <vector>
+#include <iterator>
 #include <algorithm>
-
-void getValues(std::vector<int>& myArray){
-  std::cout << "Enter " << myArray.size() << " elements\n";
-  for(int i = 0; i < myArray.size(); i++){
-    std::cin >> myArray[i];
-  }
-}
-void printValues(std::vector<int>& myArray){
-  for(int i = 0; i < myArray.size(); i++){
-    std::cout << myArray[i] << " ";
-  }
-  std::cout << std::endl;
-}
-
 int main(){
-  unsigned int arr_size_1, arr_size_2;
-  std::cout << "Enter size of first array: ";
-  std::cin >> arr_size_1;
-  std::cout << "Enter size of second array: ";
-  std::cin >> arr_size_2;
-
-  std::vector<int> myArray_1(arr_size_1);
-  std::vector<int> myArray_2(arr_size_2);
-
-  getValues(myArray_1);
-  getValues(myArray_2);
-
+  std::vector<int> myArray_1,myArray_2;
+  std::cout << "Enter elements of first array: (Enter Ctrl+ D to end.)\n";
+  std::copy(std::istream_iterator<int> {std::cin}, std::istream_iterator<int>{},
+        std::back_inserter(myArray_1));
+  std::cin.clear();
   std::sort(myArray_1.begin(), myArray_1.end());
-  std::sort(myArray_2.begin(), myArray_2.end());
-
   auto iter1 = std::unique(myArray_1.begin(), myArray_1.end());
+  myArray_1.erase(iter1, myArray_1.end());
+
+  std::cout << "Enter elements of second array: (Enter Ctrl + D to end.)\n";
+  std::copy(std::istream_iterator<int> {std::cin}, std::istream_iterator<int>{},
+        std::back_inserter(myArray_2));
+  std::sort(myArray_2.begin(), myArray_2.end());
   auto iter2 = std::unique(myArray_2.begin(), myArray_2.end());
-
-  myArray_1.resize(std::distance(myArray_1.begin(), iter1));
-  myArray_2.resize(std::distance(myArray_2.begin(), iter2));
-
-
+  myArray_2.erase(iter2, myArray_2.end());
+  
+  /* print the identical numbers */
   std::cout << "Final Output: \n";
   int f = 0;
   for(auto& val : myArray_1)
