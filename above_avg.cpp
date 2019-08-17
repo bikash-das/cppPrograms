199a3aed3bea1b6dc55c25a2e82573952b198a9a
Bikash Das <dasbikash989@gmail.com>
2019-08-17 13:57:05 +0530

    Marks are generated randomly

--- above_avg.cpp
+++ above_avg.cpp
@@ -1,18 +1,34 @@
-/* count how many students marks are above average */
+/* Count no of student above average */
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-int getAvg(std::vector<int>& vec){
-  double avg {0.0};
-  avg = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
-  std::cout << "Average: " << avg << std::endl;
-  int count = std::count_if(vec.begin(), vec.end(), [&](int i){return i > avg;});
+#include <vector>       /* for vector */
+#include <algorithm>    /* for accumulate() and count_if() */
+#include <random>       /* for random number generation */
 
-  return count;
+int count_above(const std::vector<int>& vec){
+  double avg {0.0};
+  /* find average marks */
+  avg = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+  /* count marks above average and return */
+  return std::count_if(vec.begin(), vec.end(), [&](int i){return i > avg;});
+}
+void print(const std::vector<int>& marks){
+    std::cout << "Random Marks: ";
+    for(auto &m : marks)
+        std::cout << m << " ";
+    std::cout << "\n";
+}
+void generate_marks(std::vector<int>& marks,int size){
+    std::random_device rd; /* obtain a random number from hardware */
+    std::mt19937 seed(rd()); /* seed the generator */
+    std::uniform_int_distribution<> distribute(0,100); /* define range */
+    for(int i = 0; i < size; i++)
+        marks.push_back(distribute(seed));
 }
 int main(){
-  std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
-  std::cout << getAvg(vec) << std::endl;
-
+  int no_of_stud = 30;
+  std::vector<int> marks {no_of_stud};
+  generate_marks(marks, no_of_stud);
+  print(marks);
+  std::cout << "Number of students above average = "
+            << count_above(marks) << std::endl;
 }
