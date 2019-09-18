e31f8f845713d6dab7bfd4843a4957fb5e14f509
Bikash Das <dasbikash989@gmail.com>
2019-09-18 20:28:21 +0530

    Given a two sorted array, it finds the median without merging

--- CompetitiveProgramming/medianSortArray.cpp
+++ CompetitiveProgramming/medianSortArray.cpp
@@ -1,4 +1,3 @@
-// median of two sorted array with different size youtube: https://www.youtube.com/watch?v=LPFhl65R7ww
 #include <bits/stdc++.h>
 using namespace std;
 class medianSortArray{
@@ -13,24 +12,26 @@ public:
         int lenVec1 = vec1.size(), lenVec2 = vec2.size();
         int low = 0, high = lenVec1;
         while(low <= high){
-            int partitionX = (low + high) / 2;
-            int partitionY = (vec1.size() + vec2.size() + 1) / 2 - partitionX;
-            /*  edge cases : if partitionX is 0 means nothing in left side and if partitionY is x means nothing in right side */
-            int maxleftX = (partitionX == 0) ? INT_MIN : vec1[partitionX-1];
-            int minRightX = (partitionX == lenVec1) ? INT_MAX : vec1[partitionX];
-            int maxleftY = (partitionY == 0) ? INT_MIN : vec2[partitionY - 1];
-            int minRightY = (partitionY == lenVec2) ? INT_MAX : vec2[partitionY];
+            int pX = (low + high) / 2;  //partitionX
+            int pY = (vec1.size() + vec2.size() + 1) / 2 - pX;
+            /*  edge cases : if pX is 0 means nothing in 
+              left side and if pY is x means nothing in right side */
+            int maxleftX = (pX == 0) ? INT_MIN : vec1[pX-1];
+            int minRightX = (pX == lenVec1) ? INT_MAX : vec1[pX];
+            int maxleftY = (pY == 0) ? INT_MIN : vec2[pY - 1];
+            int minRightY = (pY == lenVec2) ? INT_MAX : vec2[pY];
 
             if((maxleftX <= minRightY) && (maxleftY <= minRightX)){
                 if((lenVec1+lenVec2)%2 == 0){
-                    return static_cast<double>((max(maxleftX,maxleftY) + min(minRightX,minRightY)) / 2.0);
+                    return static_cast<double>((max(maxleftX,maxleftY)
+                                + min(minRightX,minRightY)) / 2.0);
                 } else {
                     return static_cast<double>(max(maxleftX, maxleftY));
                 }
-            } else if (maxleftX > minRightY) {  //for partitionX go on left size
-                high = partitionX - 1;
+            } else if (maxleftX > minRightY) {  
+                high = pX - 1;
             } else {
-                low = partitionX  + 1;
+                low = pX  + 1;
             }
         }   
     }
