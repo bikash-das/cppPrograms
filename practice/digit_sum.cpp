acb4bac2bce92ca627265be0e46395b628e229cb
Bikash Das <dasbikash989@gmail.com>
2019-10-02 15:48:57 +0530

    s um3asdjfw

---
+++ practice/digit_sum.cpp
@@ -0,0 +1,35 @@
+/* two numbers given, find sum of digits for 1st number with the digits that doesn't present in the 2nd number?
+  eg: 137 and 17 = 3
+      137 and 7 = 1+3 = 4
+*/
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <set>
+using namespace std;
+int main(){
+    string s1, s2;
+    cin >> s1 >> s2;
+    try{
+        assert(stol(s1) > 0 && stol(s2) > 0);
+        int sum = 0;
+        set<char> char_set;
+        for(auto v : s2){
+            char_set.insert(v);
+        }
+        for(auto& v : s1){
+            if(char_set.find(v) != char_set.end())
+                continue;
+            
+            // add only if no is not in char_set
+            sum += v - '0';
+        }
+        cout << sum << "\n";
+
+    }catch(invalid_argument& e){
+        cout << "Invalid Input\n";
+    }catch(out_of_range& e){
+        cout << "long integer range exceeded\n";
+    }
+    
+}
